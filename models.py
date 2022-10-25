7f1c3e71472536942fe35eeeb69facde2b2c111e
Shutak0 <111364245+Shutak0@users.noreply.github.com>
2022-10-25 16:02:19 +0200

    Changes

--- models.py
+++ models.py
@@ -15,10 +15,10 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, db_index=True)
     def get_absolutely_url(self):
         return reverse('forum:post', args=[self.id, self.slug])
-    def get_comment(post):
-        return Comment.objects.filter(product=post)
+    def get_comments(post):
+        return Comment.objects.filter(post=post)
 class Comment(models.Model):
-    comment = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text = models.TextField(max_length=200)
     author = models.CharField(max_length=16)
     created = models.DateField(auto_now_add=True)
--- views.py
+++ views.py
@@ -1,8 +1,10 @@
 from itertools import product
 from tracemalloc import get_object_traceback
+from urllib import request
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Post
+from .models import Comment
 def index(request):
     posts = Post.objects.all()
     return render(request, 'forum/index.html', {'posts':posts})
@@ -20,4 +22,8 @@ def ckartinka(request):
     return render(request, 'img/weee.png')
 def post(request, id, slug):
     post = get_object_or_404(Post, id=id, slug=slug)
+    if request.method == 'POST':
+        client_text = request.POST.get('text', False)
+        comment = Comment(text=client_text, author='ádmín', post=post)
+        comment.save()
     return render(request, 'forum/comments.html', {'post': post})
